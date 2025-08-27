528e2e4ada73ce961d6bc65c5d0dd2a623a12c42
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-08-27 19:50:35 -0300

    Implementing logic of game in engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,21 +4,114 @@ const state = {
         computerScore: 0,
         scoreBox: document.getElementById("score-box"),
     },
+// *******************
+// Left side
+// *******************
     cardSprites:{
         avatar: document.getElementById("card-image"),
         name: document.getElementById("card-name"),
         type: document.getElementById("card-type")
     },
+// *******************
+// Right side
+// *******************
     fieldCards:{
+        // Player card in challenge
         player: document.getElementById("player-field-card"),
+        // Computer card in challenge
         computer: document.getElementById("computer-field-card"),
     },
     actions:{
         button: document.getElementById("next-duel")
     }
+};
+const playerSides = {
+    // Player cards container
+    player1: "player-cards",
+    // Computer cards container
+    computer: "computer-cards",
 }
-function init() {
 
+const pathImages = "./src/assets/icons/";
+
+// Cards informations
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2]
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0]
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1]
+    },
+]
+
+// Generate aleatory number of a card
+async function getRandomCard(){
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+
+async function creatCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard)
+        });
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(IdCard)
+    });
+
+    return cardImage;
+}
+
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Attribute: " + cardData[index].type;
+
+}
+
+async function drawCards(cardNumbers, fieldSide){
+    for(let i = 0; i < cardNumbers; i++){
+        const randomIdCard = await getRandomCard();
+        const cardImage = await creatCardImage(randomIdCard, fieldSide);
+        
+        document.getElementById(fieldSide).appendChild(cardImage)
+    }
+}
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
