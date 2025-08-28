e2db97acd96ffb43aaf46dce1cf26a1030d82292
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-08-27 22:53:51 -0300

    Adding audio in index.html and making some adjusts in code

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -204,6 +204,9 @@ async function init() {
 
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 }
 
 init();
