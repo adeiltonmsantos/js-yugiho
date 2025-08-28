e87d7fe967dbd2238f2322e332b56ea8a956b065
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-08-27 22:04:43 -0300

    Implementing rest of challenge logic

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,7 +2,7 @@ const state = {
     score:{
         playerScore: 0,
         computerScore: 0,
-        scoreBox: document.getElementById("score-box"),
+        scoreBox: document.getElementById("score_points"),
     },
 // *******************
 // Left side
@@ -112,10 +112,35 @@ async function setCardsField(cardId){
     state.fieldCards.player.src = cardData[cardId].img;
     state.fieldCards.computer.src = cardData[computerCardId].img
     
-    // let duelResults = await checkDuelResults(cardId, computerCardId)
+    let duelResults = await checkDuelResults(cardId, computerCardId)
 
-    // await updateScore();
-    // await drawButton(duelResults);
+    await updateScore();
+    await drawButton(duelResults);
+}
+
+async function drawButton(text){
+    state.actions.button.innerText = text;
+    state.actions.button.style.display = "block";
+}
+
+async function updateScore(){
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
+}
+
+async function checkDuelResults(playerCardId, computerCardId){
+    let duelResults = "Empate";
+    let playerCard = cardData[playerCardId];
+
+    if(playerCard.WinOf.includes(computerCardId)){
+        duelResults = "win";
+        state.score.playerScore++;
+    }else if(playerCard.LoseOf.includes(computerCardId)){
+        duelResults = "lose";
+        state.score.computerScore++;
+    }
+    await playAudio(duelResults);
+
+    return duelResults.toUpperCase();
 }
 
 async function drawSelectCard(index) {
@@ -132,6 +157,24 @@ async function drawCards(cardNumbers, fieldSide){
         
         document.getElementById(fieldSide).appendChild(cardImage)
     }
+}
+
+async function resetDuel() {
+    state.cardSprites.avatar.src = "";
+    state.actions.button.style.display = "none";
+
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+
+    init();
+}
+
+async function playAudio(status) {
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
+    try{
+        audio.play();
+    }
+    catch{};
 }
 
 function init() {
